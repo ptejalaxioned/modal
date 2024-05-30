1d64b6d6f62ae28d51862e07131e81433337ef23
ptejalaxioned <tejalp@axioned.com>
2024-05-30 15:43:37 +0530

    JS file commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,67 +4,35 @@ let childCount = parentDiv.children.length;
 
 li.forEach((element, index) => {
   element.addEventListener("click", () => {
-    console.log(element.children[0].children[0].src);
-let src=element.children[0].children[0].src
+    let src = element.children[0].children[0].src;
+    let content = element.children[0].children[1].innerText;
     if (parentDiv.children.length === childCount) {
-        // Modal div create here
+      // Modal div create here
       let modal = document.createElement("div");
       parentDiv.appendChild(modal);
-      modal.classList.toggle("modal")
+      modal.classList.toggle("modal");
       // figure tag create here
       let figure = document.createElement("figure");
-      modal.appendChild(figure);
-      // Style
-      figure.style.width="20%"
+      figure.classList.toggle("modal-figure");
       //img tag create here
       let img = document.createElement("img");
-      figure.appendChild(img);
-      img.setAttribute("src",src)
-      // Style
-      img.style.width="100%"
-      console.log(parentDiv,img.src);
-     
+      let figcaption = document.createElement("figcaption");
+      figure.append(img, figcaption);
+      img.setAttribute("src", src);
+      figcaption.innerText = content;
+      //close icon
+      let close = document.createElement("span");
+      close.classList.toggle("close");
+      modal.append(figure, close);
+      let modalE = document.querySelector(".modal");
+
+      modalE.addEventListener("click", (e) => {
+        console.log(e.target);
+        console.log(img);
+        if (e.target != img && e.target != figure && e.target != figcaption) {
+          parentDiv.removeChild(modal);
+        }
+      });
     }
   });
 });
-
-// <div class="modal">
-// <figure class="video-bg">
-//   <img
-//     src="./assets/images/images/about-video-bg.jpg"
-//     alt="Video"
-//   />
-// </figure>
-// <figure class="close">
-//   <img src="./assets/images/images/close.png" alt="Close" />
-// </figure>
-// </div>
-
-// .modal {
-//     width: 100%;
-//     height: 100%;
-//     display: none;
-//     position: fixed;
-//     z-index: 3;
-//     left: 0;
-//     top: 0;
-//     background-color: rgb(0, 0, 0);
-//     background-color: rgba(0, 0, 0, 0.4);
-//     overflow: auto;
-//   }
-
-//   .video-bg {
-//     width: 40%;
-//     position: absolute;
-//     top: 50%;
-//     left: 50%;
-//     transform: translate(-50%, -50%);
-//   }
-
-//   .close {
-//     width: 3%;
-//     position: absolute;
-//     z-index: 3;
-//     top: 5%;
-//     right: 5%;
-//   }
